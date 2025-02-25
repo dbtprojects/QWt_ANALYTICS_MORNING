aa4b13eee81acc6d46f849caffd3a5730d81c8f5
kumar sangisetti <connect@datadarwin.io>
2025-02-25 04:05:45 +0000

    final-commit

--- macros/get_backend_data.sql
+++ macros/get_backend_data.sql
@@ -19,7 +19,48 @@
 
     {{ return(results_list) }}
 
+{% endmacro %}
 
-    
+{% macro get_min_orderdate() %}
+
+    {% set get_min_orderdate_query %}
+
+        SELECT min(orderdate)
+        from {{ref('fct_orders')}}
+     
+    {% endset %}
+
+    {% set results = run_query(get_min_orderdate_query) %}
+
+    {% if execute %}
+    {# Return the first column #}
+    {% set results_list = results.columns[0][0] %}
+    {% else %}
+    {% set results_list = [] %}
+    {% endif %}
+
+    {{ return(results_list) }}
+
+{% endmacro %}
+
+{% macro get_max_orderdate() %}
+
+    {% set get_max_orderdate_query %}
+
+        SELECT max(orderdate)
+        from {{ref('fct_orders')}}
+     
+    {% endset %}
+
+    {% set results = run_query(get_max_orderdate_query) %}
+
+    {% if execute %}
+    {# Return the first column #}
+    {% set results_list = results.columns[0][0] %}
+    {% else %}
+    {% set results_list = [] %}
+    {% endif %}
+
+    {{ return(results_list) }}
 
 {% endmacro %}
